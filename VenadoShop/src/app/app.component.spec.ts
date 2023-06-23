f958d8657c06aabf39cc8125509af5943fbe61bc
Vanessa Quiñonez <vanesabentos89@gmail.com>
2023-06-23 17:51:54 -0300

    Eliminar cosas de la barra de navegacion

--- VenadoShop/src/app/app.component.spec.ts
+++ VenadoShop/src/app/app.component.spec.ts
@@ -2,6 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
+/* Este es un conjunto de pruebas para el `AppComponent` en una aplicación angular.La función `beforeEach`
+Establece el módulo de prueba con las importaciones y declaraciones necesarias.Las tres funciones `it` son
+Pruebas individuales que verifican si el componente de la aplicación se crea correctamente, si el título de la aplicación se
+establece en 'VenadoShop', y si el título se representa correctamente en la plantilla HTML.El `expect '
+declaraciones en cada prueba verifique si el resultado real coincide con el resultado esperado.*/
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecommerce'`, () => {
+  it(`should have as title 'VenadoShop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecommerce');
+    expect(app.title).toEqual('VenadoShop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ecommerce app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('VenadoShopapp is running!');
   });
 });
